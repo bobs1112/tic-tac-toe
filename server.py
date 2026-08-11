eb35d6d9f507ca0ab1eafaf8931d15c2567a3774
bobs1112 <kiryungas@yandex.ru>
2026-08-11 17:43:30 +0300

    We added an unfinished game against the computer; we'll finish it later.

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, jsonify, session, redirect
 from flask_session import Session
 import threading
 import time
+import random
 app = Flask("__name__")
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -17,12 +18,13 @@ crossWin = "крестики выиграли"
 nullWin = "нолики выиграли"
 Session(app)
 class Game:
-    def empty():
+    def empty(self):
         return
     
-    def __init__(self):
+    def __init__(self, itsAi):
         self.currentState = beforeStartState
         self.stop_event = threading.Event()
+        self.Ai = itsAi
         self.timer_theard = threading.Thread(target=self.empty)
 
         self.timer_theard.start()
@@ -31,7 +33,6 @@ class Game:
         self.cells = [['_', '_', '_'], 
                      ['_', '_', '_'],
                      ['_', '_', '_'],  ]
-
     def returndiagonal(self, diagonal):
     
         if diagonal == 0:
@@ -52,6 +53,46 @@ class Game:
                 acc += 1
         return acc
 
+    def aiTryToBlock(self, sym):
+        for i in range(3):
+            theRow = self.rowreturn(i)
+            if self.count(theRow, 'X') == 2 and self.count(theRow , '_') == 1:
+                column = theRow.index('_')
+                self.cells[i][column] = sym
+                return True
+        for i in range(3):
+            theColumn = self.returncolumn(i)
+            if self.count(theColumn, 'X') == 2 and self.count(theColumn , '_') == 1:
+                row = theColumn.index('_')
+                self.cells[row][i] = sym
+                return True
+        for i in range(2):
+            TheDiagonal = self.returndiagonal(i)
+            if self.count(TheDiagonal, 'X') == 2 and self.count(TheDiagonal , '_') == 1:
+                
+                return True
+        return False
+    
+    def aiTryToWin(self, sym):
+        for i in range(3):
+            theRow = self.rowreturn(i)
+            if self.count(theRow, sym) == 2 and self.count(theRow , '_') == 1:
+                column = theRow.index('_')
+                self.cells[i][column] = sym
+                return True
+        for i in range(3):
+            theColumn = self.returncolumn(i)
+            if self.count(theColumn, sym) == 2 and self.count(theColumn , '_') == 1:
+                row = theColumn.index('_')
+                self.cells[row][i] = sym
+                return True
+        for i in range(2):
+            TheDiagonal = self.returndiagonal(i)
+            if self.count(TheDiagonal, sym) == 2 and self.count(TheDiagonal , '_') == 1:
+                
+                return True
+        return False
+    
     def exa(self, sym):
         for i in range(3):
             theRow = self.rowreturn(i)
@@ -108,11 +149,36 @@ class Game:
         if self.exa('X') :
             self.currentState = crossWin
             return
-        
-        self.currentState = nullMakeMove
-        self.stop_event.clear()
-        self.timer_thread = threading.Thread(target=self.delay_action, args=('O'))
-        self.timer_thread.start()
+
+        if self.Ai == False:
+            self.currentState = nullMakeMove
+            self.stop_event.clear()
+            self.timer_thread = threading.Thread(target=self.delay_action, args=('O'))
+            self.timer_thread.start()
+        elif self.Ai == True:
+            row = random.randint(0, 2)
+            column = random.randint(0, 2)
+            self.currentState = nullMakeMove
+            condition = self.cells[column][row] != '_'
+            tryCount = 0
+            if self.aiTryToWin('O'):
+                self.currentState = nullWin
+                return
+            if self.aiTryToBlock('O'):
+                self.currentState = crossMakeMove
+                return
+            if self.cells[1][1] == '_':
+                self.MakeMoveNull(1, 1)
+                return
+            while condition or tryCount >= 10:
+                row = random.randint(0, 2)
+                column = random.randint(0, 2)
+                condition = self.cells[column][row] != '_'
+                tryCount += 1
+            if tryCount == 10:
+                raise ValueError("The provided value is invalid.")
+            
+            self.MakeMoveNull(column, row)
     def MakeMoveNull(self, column, row):
         self.stop_event.set()
         self.timer_theard.join()
@@ -148,6 +214,24 @@ def newgame():
     global games 
     return redirect(f'/start_game/{str(len(games) + 1)}')
 
+
+@app.route('/start_ai_game')
+def AiGame():
+    global games
+    id = len(games) + 1
+    existGame = games.get(id)
+    session["role"] = 'X'
+    if existGame is None:
+        session["id"] = id
+        newGame = Game(True)
+        games[id] = newGame
+        
+        newGame.currentState = crossMakeMove
+        
+    return redirect(f'start_game/{id}')
+
+
+
 @app.route('/games')
 def returngame():
     global games
@@ -165,24 +249,28 @@ def returngame():
 
 @app.route('/start_game/<int:id>')
 def start(id) :
+
     global games
     existGame = games.get(id)
     if existGame is None:
-        newGame = Game()
+        newGame = Game(False)
         games[id] = newGame
         
         newGame.currentState = waitSecondPlayerGameState
         session["role"] = 'X'
         
         return render_template('game.html')
+    
     elif existGame.currentState == waitSecondPlayerGameState :
         session["role"] = 'O'
         existGame.currentState = crossMakeMove
         return render_template('game.html')
-            
+    
+    if existGame.Ai == True:
+        return render_template('game.html')
+    
     session["role"] = 'spectator'
     return render_template('game.html')
-
 
 @app.route('/start_game/<int:id>/cells')
 def returncells(id):
@@ -241,7 +329,7 @@ def returncells(id):
             'message' : "",
             'cells' : game.cells
         });
-    
+
 
 @app.route('/start_game/<int:id>/make_move/<int:column>/<int:row>')
 def make_move(id, column, row):
